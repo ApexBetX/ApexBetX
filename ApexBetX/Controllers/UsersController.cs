ef2592ba1b65211bdc92c8c1bb9dde11a24dc9a9
Esihle Mlinjana <EsihleM@hollywoodbets.net>
2026-07-03 11:13:06 +0200

    implement users list page with CRUD navigation

--- ApexBetX/Controllers/UsersController.cs
+++ ApexBetX/Controllers/UsersController.cs
@@ -1,10 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApexBetX.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApexBetX.Controllers
 {
     public class UsersController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _context.Users
+                .OrderBy(u => u.Surname)
+                .ToListAsync();
+
+            return View(users);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
         {
             return View();
         }
