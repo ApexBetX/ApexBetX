9546b97079e8dc95cc9114e10c2a4f0cc5e9ee85
Esihle Mlinjana <EsihleM@hollywoodbets.net>
2026-07-04 11:37:48 +0200

    prevent duplicate ID number registration with unit tests

--- ApexBetX.Tests/Services/UserServiceTests.cs
+++ ApexBetX.Tests/Services/UserServiceTests.cs
@@ -1,15 +1,27 @@
-﻿using ApexBetX.Models;
+﻿using ApexBetX.Data;
+using ApexBetX.Models;
 using ApexBetX.Services;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace ApexBetX.Tests.Services
 {
     public class UserServiceTests
     {
+        private ApplicationDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+
         [Fact]
         public void User_With_No_Accounts_Can_Be_Deleted()
         {
-            var service = new UserService();
+            var context = GetDbContext();
+            var service = new UserService(context);
 
             var user = new User
             {
@@ -22,7 +34,8 @@ namespace ApexBetX.Tests.Services
         [Fact]
         public void User_With_Open_Account_Cannot_Be_Deleted()
         {
-            var service = new UserService();
+            var context = GetDbContext();
+            var service = new UserService(context);
 
             var user = new User
             {
@@ -38,7 +51,8 @@ namespace ApexBetX.Tests.Services
         [Fact]
         public void User_With_All_Closed_Accounts_Can_Be_Deleted()
         {
-            var service = new UserService();
+            var context = GetDbContext();
+            var service = new UserService(context);
 
             var user = new User
             {
@@ -50,6 +64,40 @@ namespace ApexBetX.Tests.Services
             };
 
             Assert.True(service.CanDeleteUser(user));
+        }
+
+        [Fact]
+        public async Task IDNumberExistsAsync_Returns_True_When_IDNumber_Already_Exists()
+        {
+            var context = GetDbContext();
+
+            context.Users.Add(new User
+            {
+                IDNumber = "9901015009087",
+                FirstName = "Test",
+                Surname = "User",
+                Email = "test@test.com",
+                Phone = "0712345678"
+            });
+
+            await context.SaveChangesAsync();
+
+            var service = new UserService(context);
+
+            var result = await service.IDNumberExistsAsync("9901015009087");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task IDNumberExistsAsync_Returns_False_When_IDNumber_Does_Not_Exist()
+        {
+            var context = GetDbContext();
+            var service = new UserService(context);
+
+            var result = await service.IDNumberExistsAsync("8801015009087");
+
+            Assert.False(result);
         }
     }
 }
--- ApexBetX/Services/UserService.cs
+++ ApexBetX/Services/UserService.cs
@@ -1,14 +1,26 @@
-﻿using ApexBetX.Models;
+﻿using ApexBetX.Data;
+using ApexBetX.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApexBetX.Services
 {
     public class UserService
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public bool CanDeleteUser(User user)
         {
             return user.BettingAccounts == null ||
                    !user.BettingAccounts.Any() ||
                    user.BettingAccounts.All(a => a.IsClosed);
+        }
+        public async Task<bool> IDNumberExistsAsync(string idNumber)
+        {
+            return await _context.Users.AnyAsync(u => u.IDNumber == idNumber);
         }
     }
 }
