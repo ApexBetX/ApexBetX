2671b750d719d01cc04158050b15711918559885
Esihle Mlinjana <EsihleM@hollywoodbets.net>
2026-07-04 18:46:03 +0200

    prevent duplicate account numbers with service validation and unit tests

--- ApexBetX/Services/AccountService.cs
+++ ApexBetX/Services/AccountService.cs
@@ -1,9 +1,18 @@
-﻿using ApexBetX.Models;
+﻿using ApexBetX.Data;
+using ApexBetX.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApexBetX.Services
 {
     public class AccountService
     {
+        private readonly ApplicationDbContext _context;
+
+        public AccountService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public bool CanCloseAccount(BettingAccount account)
         {
             return account.Balance == 0;
@@ -12,6 +21,12 @@ namespace ApexBetX.Services
         public bool CanAddTransaction(BettingAccount account)
         {
             return account.IsClosed == false;
+        }
+
+        public async Task<bool> AccountNumberExistsAsync(string accountNumber)
+        {
+            return await _context.BettingAccounts
+                .AnyAsync(a => a.AccountNumber == accountNumber);
         }
     }
 }
